81b57d097261fc3876cb4ea02cb913ba1b597103
Name <email@example.ru>
2022-09-01 10:41:42 +0300

    Create Tik-tak-toe.py

---
+++ Tik-tak-toe.py
@@ -0,0 +1,76 @@
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+player = 1
+
+win = 1
+draw = -1
+running = 0
+stop = 1
+
+game = running
+mark = 'X'
+
+def DrawBoard():
+    print(" %c | %c | %c " % (board[1],board[2],board[3]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[4],board[5],board[6]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[7],board[8],board[9]))
+    print("   |   |   ")
+
+def CheckPosition(x):
+    if(board[x] == ' '):
+        return True
+    else:
+        print("Выбранная позиция уже занята! Повторите ваш выбор.")
+        return False
+
+def CheckWin():
+    global game
+    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):
+        game = win
+    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):
+        game = win
+    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):
+        game = win
+    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):
+        game = win
+    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
+        game = win
+    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):
+        game=win
+    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):
+        game = win
+    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):
+        game=win
+    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):
+        game=Draw
+    else:
+        game=running
+
+print("Player 1 [X] --- Player 2 [O]\n")
+print()
+print()
+print("Please Wait...")
+
+while(game == running):
+    DrawBoard()
+    if(player % 2 != 0):
+        print("Ходит первый игрок")
+        mark = 'X'
+    else:
+        print("Ходит второй игрок")
+        mark = 'O'
+    choice = int(input("Введите число от 1 до 9, чтобы поставить свою фишку: "))
+    if(CheckPosition(choice)):
+        board[choice] = mark
+        player+=1
+        CheckWin()
+DrawBoard()
+if(game==draw):
+    print("Ничья!")
+elif(game==win):
+    player-=1
+    if(player%2!=0):
+        print("Первый игрок победил!")
+    else:
+        print("Второй игрок победил!")
